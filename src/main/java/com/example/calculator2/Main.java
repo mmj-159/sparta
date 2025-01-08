621a5d797f07e7fa0259497f3344f7deae4e88ec
mmj <myoungminjun@gmail.com>
2025-01-08 14:46:45 +0900

    계산기 레벨2 1차 수정

--- src/main/java/com/example/calculator2/Main.java
+++ src/main/java/com/example/calculator2/Main.java
@@ -4,11 +4,14 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         calculator2 cal = new calculator2();
-        while (true) {
+
+        do {
             System.out.println("첫번째 숫자 입력 (문자를 입력시 오류발생):");
+
             int firstNum = sc.nextInt();
 
             System.out.print("사칙연산 기호를 입력하세요: ");
@@ -18,14 +21,10 @@ public class Main {
             System.out.print("두번째 숫자 입력 (문자를 입력시 오류발생):");
             int secondNum = sc.nextInt();
 
-            System.out.println(cal.calculate2(sign,firstNum,secondNum));
+            System.out.println(cal.calculate2(sign, firstNum,secondNum));
+            System.out.println("계속하시겠습니까?(exit를 입력하면 프로그램이 종료됩니다.) :");
 
-            System.out.println("계속하시겠습니까?(NO를 입력하면 프로그램이 종료됩니다.) :");
-
-            if (Objects.equals(sc.next(), "no") || Objects.equals(sc.next(), "No")|| Objects.equals(sc.next(), "NO")) {
-                System.out.println("프로그램 종료.");
-                break;
-            }
-        }
+        } while (!sc.next().equals("exit"));
+        System.out.println("프로그램 종료.");
     }
 }
--- src/main/java/com/example/calculator2/calculator2.java
+++ src/main/java/com/example/calculator2/calculator2.java
@@ -6,25 +6,41 @@ public class calculator2 {
     private double result;
 
     public double calculate2(String sign, int firstNum, int secondNum) {
-        if (Objects.equals(sign,"+")) {
-            result = firstNum + secondNum;
-            System.out.println("결과 : " + result);
-        } else if (Objects.equals(sign,"-")) {
-            result = firstNum - secondNum;
-            System.out.println("결과 : " + result);
-        } else if (Objects.equals(sign,"*")) {
-            result = firstNum * secondNum;
-            System.out.println("결과 : " + result);
-        } else if (Objects.equals(sign,"/")) {
-            if (secondNum == 0) {
-                System.out.println("0으로 나눌 수 없습니다.");
-            } else {
+        switch (sign) {
+            case "+":
+                result = firstNum + secondNum;
+                break;
+            case "-":
+                result = firstNum - secondNum;
+                break;
+            case "*":
+                result = firstNum * secondNum;
+                break;
+            case "/":
+                if (secondNum == 0) {
+                    System.out.println("0으로 나눌수 없습니다.");
+                    break;
+                }
                 result = (double) firstNum / secondNum;
-                System.out.println("결과 : " + result);
-            }
-        } else {
-            System.out.println("+-*/중에 입력하세요.");
+                break;
         }
+
+
+        //        if (Objects.equals(sign,"+")) {
+//            result = firstNum + secondNum;
+//        } else if (Objects.equals(sign,"-")) {
+//            result = firstNum - secondNum;
+//        } else if (Objects.equals(sign,"*")) {
+//            result = firstNum * secondNum;
+//        } else if (Objects.equals(sign,"/")) {
+//            if (secondNum == 0) {
+//                System.out.println("0으로 나눌 수 없습니다.");
+//            } else {
+//                result = (double) firstNum / secondNum;
+//            }
+//        } else {
+//            System.out.println("+-*/중에 입력하세요.");
+//        }
 
         return result;
 
